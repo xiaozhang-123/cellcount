044feccff930cd98d9be986794d2dc4b03bb62f1
xiaozhang-123 <3499656821@qq.com>
2022-08-31 13:56:04 +0800

    remove secrete key

--- xianweijing-python/CellCounter-django/djangoProject/djangoProject/settings.py
+++ xianweijing-python/CellCounter-django/djangoProject/djangoProject/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'k8x1)nhbo0j3li)y6bg60zx0g!1_r#i+@2gz6x6-cp38_v%&h2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
